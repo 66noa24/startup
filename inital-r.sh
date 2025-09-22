80363ca0f8906cbae259419fa78813ebab5eeda1
66noa24 <noahmk4605@protonmail.com>
2025-09-22 16:28:24 -0400

    Inital-r working commit

--- inital-r.sh
+++ inital-r.sh
@@ -29,7 +29,7 @@ blk_action() {
 prefs_list() {
   blk_prompt "Do you want to update the system?" upSys
   blk_prompt "Do you want to install deps?" inDeps
-  blk_prompt "Do you want to also install Docker?" "$inDocker"
+  blk_prompt "Do you want to also install Docker?" inDocker
 }
 
 update_system() {
