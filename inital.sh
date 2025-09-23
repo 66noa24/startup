7916acfe2b026fffb5254aae9d83ce9a3b070cea
66noa24 <noahmk4605@protonmail.com>
2025-09-22 20:27:28 -0400

    finish inital.sh v2

--- inital.sh
+++ inital.sh
@@ -1,76 +1,69 @@
 #!/bin/bash
-# Written on Sep 8, '25
-# Last update 09-09-25
+# Re-writen on sep 22, '25
 
 noinpt="No input, skipping"
 
-pref_list() {
-  echo "Welcome! Please select your prefrences:"
-  echo "Do you want to update the system?"
-  read -r -p "y/n: " upSys
-  case "$upSys" in
-    y) echo "Makred to update" ;; # valid
-    n) echo "Marked to skip" ;; # valid responce 
-    *) echo "$noinpt"
-  esac
-  echo "Do you want to install deps?"
-  read -r -p "y/n: " inDeps
-  case "$inDeps" in
-    y) echo "Marked for install" ;; # valid responce 
-    n) echo "Marked for skipped" ;;
-    *) echo "$noinpt" ;;
-  esac
-
-  read -r -p "Install docker also? (y/n): " inDepsDocker
-  case "$inDepsDocker" in
-    y) echo "Marked docker for install" ;; # valid responce 
-    n) echo "Marked docker for skip" ;;
-    *) echo "$noinpt" ;;
+blk_prompt() {
+  local prompt_msg=$1
+  local end_var=$2
+  echo "$prompt_msg"
+  read -r -p "(y/n): " input
+  case "$input" in
+    "y") eval "$end_var='y'" ;;
+    "n") eval "$end_var='n'" ;;
+    *) echo "$noinpt"; eval "$end_var='n'" ;;
   esac
 }
 
-sys_update() {
-  read -r -p "Cont? (with the sys update?) (y/n) " upCont
-  case "$upCont" in
-    y) sudo xbps-install -Syu ;;
-    n) echo "Skipping" ;;
-    *) echo "$noinpt" ;;
+blk_action() {
+  local choice=$1 # upSys, etc
+  local actionP=$2 # Yes action
+  local actionN=$3 # No actioin
+  case "$choice" in # read upSys var for yes or no
+    "y") eval "$actionP" ;;
+    "n") eval "$actionN" ;;
+    *) echo "$noinput" ;;
   esac
-    }
+}
 
-install_dep() {
-  read -r -p "Cont? (with dep inst?) (y/n) " depCont
-  case "$depCont" in
-    y) sudo xbps-install -Syu i3 conky polybar emptty helix kitty nitrogen dmenu libatomic x11vnc lynx ;;
-    n) echo "Skipping dep install..." ;;
-    *) echo "$noinpt" ;;
-  esac
-  }
-  
-install_dock() {
-  case "$inDepsDocker" in
-    y) echo "Installing docker..." && sudo xbps-install docker ;;
-    n) echo "Skipping docker..." ;;
-    *) echo "$noinpt" ;;
-  esac
-  }
+prefs_list() {
+  blk_prompt "Do you want to update the system?" upSys
+  blk_prompt "Do you want to install deps?" inDeps
+  blk_prompt "Do you want to also install Docker?" inDocker
+}
 
-# Main logic
-pref_list
-case "$upSys" in
-  y) sys_update && echo "Update succesfull!";;
-  n) echo "No update chosen" ;;
-  *) echo "       ";;
-esac
+update_system() {
+  if [ $upSys = "y" ]; then
+    blk_prompt "Continue with update?" upSys
+    blk_action "$upSys" "sudo xbps-install -Syu && echo '--> Updated system!'" "Skipping system update..."
+  else
+    echo "Skipped system update."
+  fi
+}
 
-case "$inDeps" in
-  y) install_dep && echo "Installed deps!";;
-  n) echo "No deps chosen" ;;
-  *) echo "       "
-esac
+dep_install() {
+  if [ $inDeps = "y" ]; then
+    blk_prompt "Do you want to continue installing deps?" inDeps
+    blk_action "$inDeps" "sudo xbps-install -Syu i3 conky polybar emptty helix kitty nitrogen dmenu libatomic x11vnc lynx && echo '--> Installed Deps!'" "echo 'Skipping dep install...'"
+  else
+    echo "Skipped dep install."
+  fi
+}
 
-case "$inDepsDocker" in
-  y) install_dock && echo "Installed docker!";;
-  n) echo "No deps chosen" ;;
-  *) echo "       "
-esac
+docker_install() {
+  if [ $inDocker = "y" ]; then
+    blk_prompt "Do you want to continue install docker?" inDocker
+    blk_action "$inDocker" "sudo xbps-install docker" "echo 'Skipping docker install...'"
+    blk_action "$inDocker" "sudo ln -s /etc/sv/docker /var/service/ && echo '--> Installed docker!'" ""
+  else
+    echo "Skipped docker install."
+  fi
+}
+
+# Function calls
+prefs_list
+update_system
+dep_install
+docker_install
+
+echo "---> Installation completed!"
