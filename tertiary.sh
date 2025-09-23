1db895926b3f541457bfda75da7ca2b6350aa94f
66noa24 <noahmk4605@protonmail.com>
2025-09-22 20:40:59 -0400

    Update tertiary working commit

--- tertiary.sh
+++ tertiary.sh
@@ -24,7 +24,7 @@ bedrock_verify() {
 }
 
 docker_verify() {
-  if sudo docker info >/dev/null 2>&1; then
+  if ls /var/service/ | grep -q docker; then
     echo "Docker is installed!"
     dockerinst=true
   else
